3725344a1394391fffe17f211dbfc142cbc80f90
johnconnor77 <jfcalleh@unal.edu.co>
2022-02-25 18:06:54 -0500

    Update for gadget processing extraction

--- libraries/gobpe/gobpe.py
+++ libraries/gobpe/gobpe.py
@@ -1,16 +1,54 @@
 from libraries.common import act_on_element, capture_page_screenshot, log_message, check_file_download_complete, files
 from config import OUTPUT_FOLDER
+import pandas as pd
 
 
 class GobPe:
 
-    def __init__(self, rpa_selenium_instance, credentials: dict):
+    def __init__(self, rpa_selenium_instance):
         self.browser = rpa_selenium_instance
-        self.infogob_url = "https://www.gob.pe/"
+        self.gobpe_url = "https://www.gob.pe/"
         self.data_dict_list = []
 
-    def access_infogob(self):
+    def access_gobpe(self):
         """
         Access Infogob from the browser.
         """
         self.browser.go_to(self.gobpe_url)
+
+    def steps_going_to_category(self, category_filename):
+        """
+
+        """
+        act_on_element("//a[@data-origin='footer-link']", "click_element")
+        act_on_element("//a[@class='power-card']/descendant::div[contains(text(), 'Organismos Autónomos')]", "click_element")
+        act_on_element("//a[@data-origin='estado-onpe-link']", "click_element")
+
+        with open(category_filename) as f:
+            lines = f.readlines()
+
+        category = lines[0]
+
+        act_on_element(f"//a[@data-origin='onpe-menu-institutional' and contains(text(),'{category}')]", "click_element")
+        act_on_element("//a[@data-origin='onpe-publicaciones-ver-más-link' and contains(text(),'Buscar informes y publicaciones')]","click_element")
+
+    def extract_info_onpe(self, date_since, date_to):
+        """
+
+        """
+        print(date_since)
+        self.browser.go_to(f"https://www.gob.pe/busquedas?contenido[]=publicaciones&desde={date_since}&hasta={date_to}&institucion[]=onpe&sheet=1&sort_by=recent")
+
+        data = pd.read_excel("Files_To_Download.xlsx")
+
+        print(data)
+
+        elements_download = data[data.DownloadRequired == "Yes"].Name.tolist()
+
+        for element_download in elements_download:
+            xpath_2_name = f"//article[@class='bg-blue-300 mt-4 p-8']//a[contains(text(),'{element_download}')]"
+            xpath_2_top_parent = "/parent::h3/parent::div/parent::div/parent::div/following-sibling::div"
+            xpath_2_button = "/a[@class='bg-transparent border-2 border-blue-700 font-bold py-2 px-3 text-blue-700 flex']"
+            act_on_element(xpath_2_name+xpath_2_top_parent+xpath_2_button, "click_element")
+
+
--- libraries/process.py
+++ libraries/process.py
@@ -1,10 +1,11 @@
 from libraries.common import log_message, capture_page_screenshot, browser
 from config import OUTPUT_FOLDER
 from libraries.gobpe.gobpe import GobPe
+import datetime
 
 
 class Process:
-    def __init__(self, credentials: dict):
+    def __init__(self):
         log_message("Initialization")
         prefs = {
             "profile.default_content_setting_values.notifications": 2,
@@ -14,18 +15,20 @@ class Process:
             "plugins.always_open_pdf_externally": True,
             "download.prompt_for_download": False
         }
-        browser.open_available_browser(preferences = prefs)#browser_selection=["firefox"]
+        browser.open_available_browser(preferences = prefs, browser_selection=["firefox"])#
         browser.set_window_size(1920, 1080)
         browser.maximize_browser_window()
 
-        gob_pe = GobPe(browser, {"url": "https://infogob.jne.gob.pe/Eleccion/FichaEleccion/segunda-vuelta-de-elecci%C3%B3n-presidencial-2016-presidencial_candidatos-y-resultados_7p675u8oH7Q=6u"})
-
-
-
-
-
     def start(self):
-        pass
+
+        log_message("Go to Peruvian Government WebPage")
+        gob_pe = GobPe(browser)
+        gob_pe.access_gobpe()
+        log_message("Go to Cageroy inside .txt file")
+        gob_pe.steps_going_to_category(category_filename="Category.txt")
+        log_message("Extract Info from certain dates")
+        gob_pe.extract_info_onpe(date_since=datetime.datetime(2021, 10, 28).strftime("%d-%m-%Y"),
+                                 date_to=datetime.datetime.now().strftime("%d-%m-%Y"))
 
     def finish(self):
         log_message("DW Process Finished")
